8da72fcc889cbbc79c46b21dab5581251bda08c3
mccaffertycr <mccaffertycr@gmail.com>
2018-06-22 22:19:29 -0400

    slight html adjusts and app.js vars declared

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// global vars
+
+var player1 = null;
+var player2 = null;
+var player1Name = '';
+var player2Name = '';
+var yourPlayerName = '';
+var player1Choice = '';
+var player2Choice = '';
+var turn = 1;
+var database = firebase.database();
