7b56e28b5e8984c9d17c6fd68e4982bf5817e393
mccaffertycr <mccaffertycr@gmail.com>
2018-06-22 21:41:06 -0400

    index.html created for the most part and firebase created

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDXaliVMtC95UOcVCUKSJfztdL6TnBia8w",
+    authDomain: "rps-multi-ba1f5.firebaseapp.com",
+    databaseURL: "https://rps-multi-ba1f5.firebaseio.com",
+    projectId: "rps-multi-ba1f5",
+    storageBucket: "",
+    messagingSenderId: "820938777472"
+  };
+  firebase.initializeApp(config);
